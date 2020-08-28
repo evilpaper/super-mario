5ce5f614f56a6502a473c169370047c66b4b3f2c
evilpaper <pellburt@gmail.com>
2020-08-28 21:55:54 +0200

    Add defineTile method to SpriteSheet class

--- public/js/main.js
+++ public/js/main.js
@@ -14,7 +14,7 @@ function drawBackground(background, context, sprites) {
 function loadMarioSprite() {
   return loadImage("./img/characters.gif").then((image) => {
     const sprites = new SpriteSheet(image, 16, 16);
-    sprites.define("idle", 17, 3);
+    sprites.define("idle", 276, 44, 16, 16);
     return sprites;
   });
 }
@@ -22,8 +22,8 @@ function loadMarioSprite() {
 function loadBackgroundSprites() {
   return loadImage("./img/tiles.png").then((image) => {
     const sprites = new SpriteSheet(image, 16, 16);
-    sprites.define("ground", 0, 0);
-    sprites.define("sky", 3, 23);
+    sprites.defineTile("ground", 0, 0);
+    sprites.defineTile("sky", 3, 23);
     return sprites;
   });
 }
