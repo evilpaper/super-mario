a99be9b5d515a7d2cb79e8ed719f06711f7d97fd
evilpaper <pellburt@gmail.com>
2020-08-30 08:10:07 +0200

    Draw Mario, add temporary update loop, add Compositor class to draw in layers

--- public/js/main.js
+++ public/js/main.js
@@ -14,13 +14,60 @@ function drawBackground(background, context, sprites) {
 const canvas = document.getElementById("screen");
 const context = canvas.getContext("2d");
 
+class Compositor {
+  constructor() {
+    this.layers = [];
+  }
+  draw(context) {
+    this.layers.forEach((layer) => {
+      layer(context);
+    });
+  }
+}
+
+function createBackgroundLayer(backgrounds, sprites) {
+  const buffer = document.createElement("canvas");
+  buffer.width = 256;
+  buffer.height = 240;
+  backgrounds.forEach((background) => {
+    drawBackground(background, buffer.getContext("2d"), sprites);
+  });
+
+  return function drawBackgroundLayer(context) {
+    context.drawImage(buffer, 0, 0);
+  };
+}
+
+function createSpriteLayer(sprite, pos) {
+  return function drawSpriteLayer(context) {
+    sprite.draw("idle", context, pos.x, pos.y);
+  };
+}
+
 Promise.all([
   loadMarioSprite(),
   loadBackgroundSprites(),
   loadLevel("1-1"),
 ]).then(([marioSprite, sprites, level]) => {
-  level.backgrounds.forEach((background) => {
-    drawBackground(background, context, sprites);
-  });
-  marioSprite.draw("idle", context, 64, 64);
+  const comp = new Compositor();
+  const backgroundLayer = createBackgroundLayer(level.backgrounds, sprites);
+
+  comp.layers.push(backgroundLayer);
+
+  const pos = {
+    x: 64,
+    y: 64,
+  };
+
+  const spriteLayer = createSpriteLayer(marioSprite, pos);
+  comp.layers.push(spriteLayer);
+
+  function update() {
+    comp.draw(context);
+    pos.x += 2;
+    pos.y += 2;
+    requestAnimationFrame(update);
+  }
+
+  update();
 });
